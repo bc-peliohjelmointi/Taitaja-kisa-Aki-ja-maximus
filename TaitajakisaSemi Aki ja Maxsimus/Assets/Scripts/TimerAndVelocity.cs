03ff688993c0a0bcf11d38be13134aa28e94794b
Struggler <116081614+Stru66ler@users.noreply.github.com>
2026-01-29 10:49:49 +0200

    ADded vel and tiem

--- TaitajakisaSemi Aki ja Maxsimus/Assets/Scripts/TimerAndVelocity.cs
+++ TaitajakisaSemi Aki ja Maxsimus/Assets/Scripts/TimerAndVelocity.cs
@@ -1,26 +1,30 @@
 using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
 
 public class TimerAndVelocity : MonoBehaviour
 {
     private Rigidbody rb;    
-    public TextMeshProUGUI Tier;
+    public TextMeshProUGUI timerText;
     public TextMeshProUGUI Velo;
 
-    public float timerDuration;
+    public float Timer;
 
-    void awake()
+    void Awake()
     {
         rb = GetComponent<Rigidbody>();
-
-
-
-
-
     }
     void Update()
     {
-        timerDuration += Time.deltaTime;
+        Timer += Time.deltaTime;
 
-        
+        int minutes = Mathf.FloorToInt(Timer / 60f);
+        int seconds = Mathf.FloorToInt(Timer % 60f);
+        int milliseconds = Mathf.FloorToInt((Timer * 1000f) % 1000f);
+
+        timerText.text = $"{minutes:00}:{seconds:00}.{milliseconds}";
+
+        float speed = rb.linearVelocity.magnitude;
+        Velo.text = $"V: {speed:F2}";
     }
 }
